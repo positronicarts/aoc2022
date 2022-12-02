f38a8ffc9452c8d67bbb5bdc3ddd360a59ac719e
Murray Rogers <murray@nplan.io>
2022-12-02 11:21:04 +0000

    Day 2 initial cut

---
+++ src/day2.rs
@@ -0,0 +1,63 @@
+pub struct Day2;
+
+#[derive(Debug)]
+enum RPSOptions {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl RPSOptions {
+    fn parse(c: char) -> Self {
+        match c {
+            'A' | 'X' => RPSOptions::Rock,
+            'B' | 'Y' => RPSOptions::Paper,
+            'C' | 'Z' => RPSOptions::Scissors,
+            _ => panic!("Invalid character {c}")
+        }
+    }
+
+    fn individual_score(&self) -> i64 {
+        match self {
+            RPSOptions::Rock => 1, 
+            RPSOptions::Paper => 2,
+            RPSOptions::Scissors => 3,
+        }
+    }
+
+    fn score_against(&self, other: &Self) -> i64 {
+        let win = matches!((self, other), 
+            (RPSOptions::Rock, RPSOptions::Paper) | 
+            (RPSOptions::Paper, RPSOptions::Scissors) |
+            (RPSOptions::Scissors, RPSOptions::Rock));
+
+        let draw = matches!((self, other),
+            (RPSOptions::Rock, RPSOptions::Rock) | 
+            (RPSOptions::Paper, RPSOptions::Paper) |
+            (RPSOptions::Scissors, RPSOptions::Scissors));
+
+        self.individual_score() + if win { 6 } else if draw { 3 }  else { 0 } 
+    }
+}
+
+impl aoc22::DayInner<Day2, i64> for Day2 {
+    fn day(&self) -> i32 {
+        2
+    }
+
+    fn inner(&self, input: String) -> (i64, i64) {
+
+        let lines: Vec<&str> = input.lines().collect();
+        println!("{}", lines.len());
+        let score: i64 = lines.iter().map(|line| {
+            let p1 = RPSOptions::parse(line.chars().next().unwrap());
+            let p2 = RPSOptions::parse(line.chars().nth(2).unwrap());
+            (p1, p2)
+        }).map(|(p1, p2)| {
+            p2.score_against(&p1)
+        }).sum();
+
+        // And we're done!
+        (score, 0)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 mod day1;
+mod day2;
 
 use aoc22::Day;
 
 fn main() {
-    day1::Day1 {}.run();
+    // day1::Day1 {}.run();
+    day2::Day2 {}.run();
 }
 
 #[cfg(test)]
 mod test {
     use super::{
-        day1, Day,
+        day1, day2, Day,
     };
 
     #[test]
     fn day1() {
         assert_eq!(day1::Day1 {}.test(), (24000, 45000));
+    }
+
+    #[test]
+    fn day2() {
+        assert_eq!(day2::Day2 {}.test(), (15, 0));
     }
 }
